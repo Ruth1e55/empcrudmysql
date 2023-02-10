3230136e4c0be95de97082df2e00ff593c40292e
vsirvi <vsirvi@rdalabs.com>
2023-02-10 17:49:59 +0530

    Initial Commit.

---
+++ src/test/java/emp/crud/EmpcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package emp.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
